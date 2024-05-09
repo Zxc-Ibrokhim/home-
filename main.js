16f6863c0024f60f864fa08792776eb711c35b97
Zxc-Ibrokhim <161280335+Zxc-Ibrokhim@users.noreply.github.com>
2024-05-09 10:49:25 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+// var ism = "polat";
+// ism = "ibrohim";
+// var ism = "Abduraxmon";
+
+
+// let ism = "polat"
+// ism = "ibrohim"
+//  let ism = "Abduraxmon"
+// console.log(ism)
+
+// const   ism ="polat"
+// //  ism = "ibrohim"              
+// console.log(ism) 
+// let userName = prompt("Please enter your name")
+// let userSurName = prompt("Please enter your Surnemember")
+
+// console.log(userName   + userS   urName)
+// let usernum1 = +prompt ("Please enter your num1");
+// let usernum2 = +prompt ("Please enter your num2");
+// console.log(usernum1-usernum2);
+
+// let userName = prompt("ism kiriting")
+// let userSurName = prompt("familiyangizni kirinting")
+// let jajax = alert("malumotlaringiz uchun raxmat ")
+// let javoblar = alert(`ism:${userName} ` )
+
+let userName = prompt("Введите текст в виде string")
+console.log(userName);
+let userNum = +prompt("Введите числа в виде number")
+console.log(userNum);
+let userBoolean = prompt("Введите текст в виде boolean")
+console.log(userBoolean);
+
+let register = confirm  ("Убедитесь что всё то что введено вами правильно")
+
+
+// alert( userBoolean, userName , UserNum)
+alert(`
+Вы ввели (${userName}) в виде string
+Вы ввели (${userNum})  в виде number 
+Вы ввели (${userBoolean}) в виде boolean`)
+
+
+
